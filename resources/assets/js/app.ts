3efef317747e4dc26042d8d33746c0f1c98f5d6f
Titas Gailius <titas.gailius@gmail.com>
2018-03-23 19:58:51 +0200

    Setup Vuex.Store

--- resources/assets/js/app.ts
+++ resources/assets/js/app.ts
@@ -9,11 +9,13 @@ import "./bootstrap"
 import Vue from "vue"
 import ExampleComponent from "./components/ExampleComponent.vue"
 import router from "./router"
+import store from "./store"
 
 Vue.component('example', ExampleComponent)
 
 new Vue({
     router,
+    store,
 
     el: '#app'
 })
